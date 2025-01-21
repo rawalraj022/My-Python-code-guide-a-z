3aab0f66cdc7ca387fb26917a41421007e56e1ce
Rajkumar Rawal <rawalraj022@gmail.com>
2025-01-21 11:29:46 +0545

    update

--- 8) Virtual Environment & requirements/61.requirements.txt.py
+++ 8) Virtual Environment & requirements/61.requirements.txt.py
@@ -1,8 +1,21 @@
-# requirements.txt in Python
+# To install all the packages listed in the requirements.txt file :
 
 """
+
 pip install -r requirements.txt
 
 """
 
+# To generates a requirements.txt file with the 
+    # current environment’s packages inside the folder  :
+
+"""
+
+pip freeze > requirements.txt
+
+"""
+
+# The pip freeze > requirements.txt command outputs the list of installed packages 
+    # and their versions into a requirements.txt file. 
+    # This file can then be used to recreate the environment.
 
